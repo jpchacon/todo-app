bac770deba8325e9131fb3cd263c8b77b08ec13a
jpchacon <j-pablo-cp@hotmail.com>
2017-12-01 16:30:07 -0500

    Tarea clase 2

--- TodoApp/app/src/main/java/com/chacon/todoapp/presentation/view/fragment/SignUpFragment.java
+++ TodoApp/app/src/main/java/com/chacon/todoapp/presentation/view/fragment/SignUpFragment.java
@@ -2,10 +2,12 @@ package com.chacon.todoapp.presentation.view.fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.chacon.todoapp.R;
@@ -16,6 +18,12 @@ import com.chacon.todoapp.presentation.view.activity.AuthActivity;
  * A simple {@link Fragment} subclass.
  */
 public class SignUpFragment extends Fragment implements SingnUpContract.View{
+
+    private TextInputLayout fullNameSignUp;
+    private TextInputLayout passwordSignUp;
+    private TextInputLayout emailSignUp;
+    private Button registroSignUp;
+    private Button iHaveAccount;
 
 
     public SignUpFragment() {
@@ -31,6 +39,14 @@ public class SignUpFragment extends Fragment implements SingnUpContract.View{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup,container,false);
+
+        fullNameSignUp = view.findViewById(R.id.fullNameSignUp);
+        passwordSignUp = view.findViewById(R.id.passwordSignUp);
+        emailSignUp = view.findViewById(R.id.emailSignUp);
+        registroSignUp = view.findViewById(R.id.registroSignUp);
+        iHaveAccount = view.findViewById(R.id.iHaveAccount);
+
+
         return view;
     }
 
